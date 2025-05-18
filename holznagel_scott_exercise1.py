227d957e6f956fbc342b0c1f5e24a88aef80b242
scottholznagel <SCOTT.HOLZNAGEL@GMAIL.COM>
2025-05-18 19:30:30 -0400

    Create holznagel_scott_exercise1.py

---
+++ holznagel_scott_exercise1.py
@@ -0,0 +1,12 @@
+# Scott Holznagel (GitHub username)
+# COP2002.0M2
+# May 18, 2025
+# Exercise 1
+# Basic printing of information
+
+name="Scott Holznagel"  # make sure to put your first and last name in the " because it is a string data type.
+print(name)
+print("COP2002.0M2")
+print("Summer 2025")
+print()  # This prints a blank line
+print("This is saved in a Python file!")
